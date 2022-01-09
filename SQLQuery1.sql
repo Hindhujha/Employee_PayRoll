21ca2a0d8a4f9131feb63de9453a8f781ef16632
unknown <hindhujhasridhar@gmail.com>
2022-01-09 11:44:00 +0530

    EMPLOYEE DETAILS ARE INSERTED INTO TABLE

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -3,10 +3,14 @@ create database payroll_service
 use payroll_service
 
 create table employee_payroll(
-Employee_ID int identity(1,1),
+Employee_ID int identity(1,1),--id is auto increment by 1
 Employee_Name varchar(255),
 Employee_Salary int,
 StartDate Date,
 )
-
+--employee details inserted into table
+INSERT INTO employee_payroll values('RAM',25000,'2021-01-02')
+INSERT INTO employee_payroll values('RAVI',23000,'2021-05-06')
+INSERT INTO employee_payroll values('ANU',22000,'2021-07-08')
+INSERT INTO employee_payroll values('ANNIE',24000,'2021-07-05')
 
