7225ba70b863acb8504dc8b021bd307ed039f174
unknown <hindhujhasridhar@gmail.com>
2022-01-09 10:58:00 +0530

    TABLE IS CREATED

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,12 @@
 create database payroll_service
 
 use payroll_service
+
+create table employee_payroll(
+Employee_ID int identity(1,1),
+Employee_Name varchar(255),
+Employee_Salary int,
+StartDate Date,
+)
+
+
