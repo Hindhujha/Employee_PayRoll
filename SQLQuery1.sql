fb1260b99d1473fdd7acfe3d264124367d788b50
unknown <hindhujhasridhar@gmail.com>
2022-01-09 12:28:01 +0530

    RETRIEVE EMPLOYEE NAMES FROM RANGES

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -15,3 +15,9 @@ INSERT INTO employee_payroll values('ANU',22000,'2021-07-08')
 INSERT INTO employee_payroll values('ANNIE',24000,'2021-07-05')
 --retrieve employee details from table
 select * from employee_payroll
+
+--retrieve particular employee detail from table
+select Employee_Salary from employee_payroll where Employee_Name='ANNIE' 
+
+--retrieve  employee detail from start dates
+select Employee_Name from employee_payroll where StartDate BETWEEN CAST('2021-05-06' AS DATE) AND  CAST('2021-07-08' AS DATE) ;
